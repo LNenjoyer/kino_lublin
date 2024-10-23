f1a920c248db01a7be2e5fdd58fdc16a2f508d65
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-10-23 08:18:23 +0200

    zmiana na zgodne z poleceniem

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import './App.css';
 import DisplayFilms from './DisplayFilms.jsx';
 import Slideshow from './slider.jsx';
 import CinemaLocation from './CinemaLocation';
-
+import Linki from './linki';
 function App() {
   return (
     <div className="App">
       {/*Header + wyszukiwanie filmów na danej stronie*/}
 
-        <div className="linki">
-            <div className="container">
-                <div className="link"><a href="https://www.cinema-city.pl/#/">ORGINAŁ</a></div>
-                <div className="link"><a href="repertuar">REPERTUAR</a></div>
-                <div className="link"><a href="oferty">OFERTY</a></div>
-                <div className="link"><a href="prezenty">PREZENTY</a></div>
-                <div className="link"><a href="bar">BAR</a></div>
-                <div className="link"><a href="szkoly">SZKOŁY</a></div>
-                <div className="link"><a href="blog">BLOG</a></div>
-                <div className="link"><a href="unlimited"><b>UNLIMITED</b></a></div>
-                <div className="link"><a href="4dx">4DX</a></div>
-                <div className="link"><a href="imax"><b>IMAX</b></a></div>
-                <div className="link"><a href="screenx">SCREEN<b>X</b></a></div>
-                <div className="link"><a href="vip"><b>VIP</b></a></div>
-            </div>
-        </div>
+       
+      <Linki/>
 
       <CinemaLocation />
 
